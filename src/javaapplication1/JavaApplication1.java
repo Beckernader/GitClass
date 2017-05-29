16f5aab2628da4a90e65d43d983e169d6ca97486
BECKERNADER <beckermarcieleduardo@gmail.com>
2017-05-29 10:12:16 -0300

    nova branch

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -5,6 +5,8 @@
  */
 package javaapplication1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Esther & Marciel
@@ -15,7 +17,9 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Scanner entrada= new Scanner(System.in);
+         System.out.println("aula de GIT modificada");
+         System.out.println("pra testar o branch");
     }
     
 }
